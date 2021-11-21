ebe56916a0d5c1fafbc837da8c4f4f682da49759
tricklynch <p@tricklynch.org>
2021-11-20 20:27:56 -0500

    Added all CIDRs specified in RFC5735

--- hakfindinternaldomains.go
+++ hakfindinternaldomains.go
@@ -16,7 +16,19 @@ func main() {
 	_, cidrA, _ := net.ParseCIDR("10.0.0.0/8")
 	_, cidrB, _ := net.ParseCIDR("172.16.0.0/12")
 	_, cidrC, _ := net.ParseCIDR("192.168.0.0/16")
-	cidrs := [3]net.IPNet{*cidrA, *cidrB, *cidrC}
+	_, cidrD, _ := net.ParseCIDR("0.0.0.0/8")
+	_, cidrE, _ := net.ParseCIDR("127.0.0.0/8")
+	_, cidrF, _ := net.ParseCIDR("169.254.0.0/16")
+	_, cidrG, _ := net.ParseCIDR("192.0.0.0/24")
+	_, cidrH, _ := net.ParseCIDR("192.0.2.0/24")
+	_, cidrI, _ := net.ParseCIDR("192.88.99.0/24")
+	_, cidrJ, _ := net.ParseCIDR("198.18.0.0/15")
+	_, cidrK, _ := net.ParseCIDR("198.51.100.0/24")
+	_, cidrL, _ := net.ParseCIDR("203.0.113.0/24")
+	_, cidrM, _ := net.ParseCIDR("224.0.0.0/4")
+	_, cidrN, _ := net.ParseCIDR("240.0.0.0/4")
+	_, cidrO, _ := net.ParseCIDR("255.255.255.255/32")
+	cidrs := [15]net.IPNet{*cidrA, *cidrB, *cidrC, *cidrD, *cidrE, *cidrF, *cidrG, *cidrH, *cidrI, *cidrJ, *cidrK, *cidrL, *cidrM, *cidrN, *cidrO}
 
 	work := make(chan string)
 	go func() {
@@ -37,7 +49,7 @@ func main() {
 	wg.Wait()
 }
 
-func doWork(work chan string, wg *sync.WaitGroup, cidrs [3]net.IPNet) {
+func doWork(work chan string, wg *sync.WaitGroup, cidrs [15]net.IPNet) {
 	defer wg.Done()
 	for text := range work {
 		ip, err := net.LookupIP(text)
